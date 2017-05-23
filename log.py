f3d8326268007ec28de7083a1f9ddaecd791e0a3
vortex-ninja <vortex.ninja@protonmail.com>
2017-05-23 23:54:34 +0200

    feat: Add function to print most popular authors

--- log.py
+++ log.py
@@ -9,4 +9,12 @@ def most_popular_articles():
         print(' * "{}" -- {} views'.format(title, views))
 
 
-most_popular_articles()
+def most_popular_authors():
+
+    results = query_database(QUERIES[1])
+    print('\nWho are the most popular article authors of all time?\n')
+    for author, views in results:
+        print(' * {} -- {} views'.format(author, views))
+
+
+most_popular_authors()
