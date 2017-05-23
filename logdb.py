0303cb0fbf91794070e8bb378c53f3bbdaf3121b
vortex-ninja <vortex.ninja@protonmail.com>
2017-05-23 23:16:00 +0200

    feat: Change function to be all purpose
    Change most_popular_articles function to be an all
    purpose database query function.
    Add closing the database connection to the function

--- logdb.py
+++ logdb.py
@@ -1,19 +1,26 @@
 import psycopg2
 
-
-def most_popular_articles():
-    """Returns three most popular articles of all time"""
-
-    conn = psycopg2.connect('dbname=news')
-    cur = conn.cursor()
-    cur.execute("""select title, count(articles.id) as num
+QUERY1 = """select title, count(articles.id) as num
                  from log, articles
                  where path = ('/article/' || articles.slug)
                  and status ='200 OK'
                  group by articles.id
                  order by num desc
-                 limit 3;""")
+                 limit 3;"""
 
+
+def query_database(q):
+    """Returns results for a given database query"""
+
+    conn = psycopg2.connect('dbname=news')
+    cur = conn.cursor()
+    cur.execute(QUERY1)
     results = cur.fetchall()
+    conn.close()
 
     return results
+
+
+# code for testing
+
+print(query_database(QUERY1))
